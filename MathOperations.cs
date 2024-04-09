a0c2694a2d69e38e054f02c3d65efe929ce4da27
Samp1997 <sampiazza2016@gmail.com>
2024-04-09 00:43:20 -0700

    Add project files.

---
+++ MathOperations.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MainMethodAssignment2
+{
+    public static class MathOperations
+    {
+        public static int Math1(int value1)
+        {
+            return value1 / 5;
+        }
+
+        public static int Math2(int value)
+        {
+            return value / 1 / 4;
+        }
+
+        public static int Math3(int value)
+        {
+            return value + 5;
+        }
+
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace MainMethodAssignment2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number");
+            int number = Convert.ToInt32(Console.ReadLine());
+            int number2 = Convert.ToInt32(Console.ReadLine());
+            int number3 = Convert.ToInt32(Console.ReadLine());
+            Console.ReadLine();
+            //Taking each int and applying to the methods
+            Console.WriteLine(MathOperations.Math1(number));
+            Console.ReadLine();
+
+            Console.WriteLine(MathOperations.Math2(number2));
+            Console.ReadLine();
+
+            Console.WriteLine(MathOperations.Math3(number.ToString(number3));
+            Console.ReadLine();
+
+        }
+    }
+}
